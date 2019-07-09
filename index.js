52beb23e0dab14f2c49d0a6c964fbcda483128a9
David Quiñones <daquinons@users.noreply.github.com>
2019-07-09 15:29:37 +0200

    Fix format

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const router =  require('./routes/api/');
+const router = require('./routes/api/');
 
 const server = express();
 server.use(express.json());
-server.use(cors())
+server.use(cors());
 server.use(router);
 
-server.listen(4000, () => console.log("Server listening at port 4000"));
+server.listen(4000, () => console.log('Server listening at port 4000'));

9e1a6b8d80b7751ef015d04f594e7190a1f3afbb
David Quiñones <daquinons@users.noreply.github.com>
2019-07-09 15:00:18 +0200

    Add post posts and put post by id

--- routes/api/posts/index.js
+++ routes/api/posts/index.js
@@ -14,6 +14,29 @@ router.get('/posts', async (req, res) => {
   }
 });
 
+router.post('/posts', async (req, res) => {
+  try {
+    const newPost = req.body;
+    if (!newPost.title || !newPost.contents) {
+      res
+        .status(400)
+        .json({
+          errorMessage: 'Please provide title and contents for the post.'
+        });
+    } else {
+      const createdPostId = await Posts.insert(newPost);
+      const createdPost = await Posts.findById(createdPostId.id);
+      res.status(201).json(createdPost[0]);
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        error: 'There was an error while saving the post to the database'
+      });
+  }
+});
+
 router.get('/posts/:id', async (req, res) => {
   try {
     const { id } = req.params;
@@ -39,6 +62,31 @@ router.delete('/posts/:id', async (req, res) => {
     if (post.length) {
       await Posts.remove(id);
       res.json(post[0]);
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'The post could not be removed' });
+  }
+});
+
+router.put('/posts/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const editedPost = { ...req.body, id };
+    const post = await Posts.findById(id);
+    if (post.length) {
+      if (editedPost.title && editedPost.contents) {
+        await Posts.update(id, editedPost);
+        const updatedPost = await Posts.findById(id);
+        res.json(updatedPost);
+      } else {
+        res.status(400).json({
+          errorMessage: 'Please provide title and contents for the post.'
+        });
+      }
     } else {
       res
         .status(404)

b579682c6bd81ff60f2ca1ac5c38e570cc7d74cf
David Quiñones <daquinons@users.noreply.github.com>
2019-07-09 14:24:40 +0200

    Add delete post by id endpoint

--- routes/api/posts/index.js
+++ routes/api/posts/index.js
@@ -32,4 +32,21 @@ router.get('/posts/:id', async (req, res) => {
   }
 });
 
+router.delete('/posts/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Posts.findById(id);
+    if (post) {
+      await Posts.remove(id);
+      res.json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'The post could not be removed' });
+  }
+});
+
 module.exports = router;

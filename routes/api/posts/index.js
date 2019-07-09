9a7ea94fe93d1c69f51c2c429836f2ff785e7869
David Quiñones <daquinons@users.noreply.github.com>
2019-07-09 14:20:23 +0200

    Add get post by id endpoint

--- routes/api/posts/index.js
+++ routes/api/posts/index.js
@@ -3,12 +3,32 @@ const Posts = require('../../../data/db');
 
 const router = express.Router();
 
-router.get("/posts", async (req, res) => {
+router.get('/posts', async (req, res) => {
   try {
     const posts = await Posts.find();
-    res.json(posts);  
+    res.json(posts);
   } catch (error) {
-    res.status(500).json({ error: "The posts information could not be retrieved." });
+    res
+      .status(500)
+      .json({ error: 'The posts information could not be retrieved.' });
+  }
+});
+
+router.get('/posts/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Posts.findById(id);
+    if (post) {
+      res.json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'The post information could not be retrieved.' });
   }
 });
 
